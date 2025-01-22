fd3d4cd428bd32e8ecd75269fce341bcc3c05176
Nicola Barbaro <barbaronico93@gmail.com>
2025-01-22 21:17:17 +0100

    Image-Picket fixed

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,33 +1,17 @@
 "use client";
 
 import classes from "@/components/meals/image-picker.module.css";
-<<<<<<< HEAD
-import { useRef } from "react";
-
-export default function ImagePicker({ label, name }) {
-=======
-<<<<<<< HEAD
-import { useRef } from "react";
-
-export default function ImagePicker({ label, name }) {
-=======
 import { useRef, useState } from "react";
 import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
   const [pickedImage, setPickedImage] = useState();
->>>>>>> 22f423f (Save meal function implemented)
->>>>>>> 074fc10 (Implemented Save function)
   const imageInput = useRef();
 
   function pickImageHandler() {
     imageInput.current.click();
   }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
   function handleImageChange(event) {
     const file = event.target.files[0];
 
@@ -45,28 +29,20 @@ export default function ImagePicker({ label, name }) {
     fileReader.readAsDataURL(file);
   }
 
->>>>>>> 22f423f (Save meal function implemented)
->>>>>>> 074fc10 (Implemented Save function)
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
         <div className={classes.preview}>
           {!pickedImage && <p>No image picked yet.</p>}
           {pickedImage && (
             <Image
+              fill
               src={pickedImage}
               alt="Picked image by the user"
-              fill
             />
           )}
         </div>
->>>>>>> 22f423f (Save meal function implemented)
->>>>>>> 074fc10 (Implemented Save function)
         <input
           className={classes.input}
           id={name}
@@ -74,14 +50,8 @@ export default function ImagePicker({ label, name }) {
           type="file"
           accept="image/png, image/jpeg"
           ref={imageInput}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
           onChange={handleImageChange}
           required
->>>>>>> 22f423f (Save meal function implemented)
->>>>>>> 074fc10 (Implemented Save function)
         ></input>
         <button
           className={classes.button}
