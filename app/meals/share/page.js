b06c386c19bfd34adea90c59fc4782a13a53b069
Nicola Barbaro <barbaronico93@gmail.com>
2025-01-22 18:23:23 +0100

    Implemented Save function

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,11 @@
+'use client';
+
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +15,10 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form
+          className={classes.form}
+          action={shareMeal}
+        >
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,72 @@
 "use client";
 
 import classes from "@/components/meals/image-picker.module.css";
+<<<<<<< HEAD
 import { useRef } from "react";
 
 export default function ImagePicker({ label, name }) {
+=======
+<<<<<<< HEAD
+import { useRef } from "react";
+
+export default function ImagePicker({ label, name }) {
+=======
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+>>>>>>> 22f423f (Save meal function implemented)
+>>>>>>> 074fc10 (Implemented Save function)
   const imageInput = useRef();
 
   function pickImageHandler() {
     imageInput.current.click();
   }
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
+>>>>>>> 22f423f (Save meal function implemented)
+>>>>>>> 074fc10 (Implemented Save function)
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="Picked image by the user"
+              fill
+            />
+          )}
+        </div>
+>>>>>>> 22f423f (Save meal function implemented)
+>>>>>>> 074fc10 (Implemented Save function)
         <input
           className={classes.input}
           id={name}
@@ -21,6 +74,14 @@ export default function ImagePicker({ label, name }) {
           type="file"
           accept="image/png, image/jpeg"
           ref={imageInput}
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+          onChange={handleImageChange}
+          required
+>>>>>>> 22f423f (Save meal function implemented)
+>>>>>>> 074fc10 (Implemented Save function)
         ></input>
         <button
           className={classes.button}
